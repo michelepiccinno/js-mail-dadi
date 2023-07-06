4ce7da3227cf32c5efb8b6c5f27a503f1f07e8ec
michelepiccinno <michele.piccinno@gmail.com>
2023-07-06 17:22:35 +0200

    add-BONUS

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /*******PARTE 1 ARRAY MAIL************/
 
 //CREO UN ARRAY CONTENENTE UN ELENCO DI INDIRIZZI E-MAIL
-const arrayMail = ["mario@gmail.com", "luca@gmail.com", "angelo@gmail.com", "grazia@gmail.com"]
+const arrayMail = ["mario@gmail.com", "luca@gmail.com", "angelo@gmail.com", "grazia@gmail.com"];
 
 //RECUPERO GLI ELEMENTI HTML INPUT E BUTTON
 const mailInputElement = document.querySelector("[name='mail']");
@@ -17,14 +17,14 @@ btnInvia.addEventListener("click", function () {
   let authorization = false;
 
   //IMPOSTO UN CICLO PER LEGGERE TUTTI GLI ELEMENTI DELL'ARRAY
-  for (i = 0; i < arrayMail.length; i++) {
+  for (let i = 0; i < arrayMail.length; i++) {
     switch (mail) {
       case arrayMail[i]:
         authorization = true;
       //CAMBIO FORZATAMENTE IL VALORE DEL CONTATORE PER USCIRE DAL CICLO
       //i = arrayMail.length;
     }
-    //ALTERNATIVA PROBABILMENTE PIU' GIUSTA PER USCIRE DAL CICLO
+    //ALTERNATIVA PROBABILMENTE PIU' CORRETTA PER USCIRE DAL CICLO
     //FUORI DALLO SWITCH MA DENTRO AL CICLO IMPOSTO:
     if (authorization == true) {
       break;
@@ -68,7 +68,7 @@ btnInviaGame.addEventListener("click", function () {
   const dadoA = Math.floor(Math.random() * (7 - 1) + 1);
   console.log('dadoA: ' + dadoA);
   const dadoB = Math.floor(Math.random() * (7 - 1) + 1);
-  console.log('dadoB: ' + dadoA);
+  console.log('dadoB: ' + dadoB);
 
   //RECUPERO GLI ELEMENTI NEI QUALI INIETTEREMO I VALORI DEI DADI GENERATI SOPRA
   document.getElementById("A-dice-value").innerHTML = dadoA;
@@ -88,4 +88,61 @@ btnInviaGame.addEventListener("click", function () {
     document.getElementById("msg-game-result").innerHTML = "IL GIOCATORE HA VINTO !"
   }
 })
+
+/********BONUS********/
+
+//RECUPERO GLI ELEMENTI HTML INPUT E BUTTON
+const myLancio1InputElement = document.getElementById("my-lancio-dado1");
+const myLancio2InputElement = document.getElementById("my-lancio-dado2");
+const myLancio3InputElement = document.getElementById("my-lancio-dado3");
+const btnInviaDadi = document.querySelector(".invia-dadi-pc");
+
+//-----PARTE GIOCATORE UTENTE------//
+//CREO UN BLOCCO DI CODICE CHE VIENE ESEGUITO SOLO QUANDO AVVIENE L'EVENTO CLICK SUL BUTTON SUL QUALE HO AGGIUNTO L'EVENTLISTENER 
+btnInviaDadi.addEventListener("click", function () {
+  const Lancio1 = myLancio1InputElement.value;
+  const Lancio2 = myLancio2InputElement.value;
+  const Lancio3 = myLancio3InputElement.value;
+  //INSERISCO GLI ELEMENTI RECUPERATI DAI 3 INPUT IN UN ARRAY
+  const arrayLanciUtente = [Lancio1, Lancio2, Lancio3];
+  //CICLO L'ARRAY PER SOMMARE I VALORI DEI SUOI ELEMENTI
+  sommaUser = 0;
+  for (let i = 0; i < arrayLanciUtente.length; i++) {
+    sommaUser += parseInt(arrayLanciUtente[i]);
+  }
+  console.log("contenuto dell'array Utente: " + arrayLanciUtente);
+  console.log('la somma dei dadi utente è: ' + sommaUser);
+  //INIETTO LA SOMMA DEI DADI UTENTE NELL'HTML
+document.getElementById("dado-utente-msg").innerHTML = sommaUser;
+
+
+//--------PARTE GIOCATORE PC------//
+//CREO UN ARRAY RANDOM DI 3 ELEMENTI PER IL COMPUTER E SOMMO IL VALORE DEI SUOI ELEMENTI
+let arrayPC = [];
+let lanciaDado = 0;
+let sommaPc = 0;
+for (i = 0; i < 3; i++) {
+  lanciaDado = Math.floor(Math.random() * (7 - 1) + 1);
+  arrayPC.push(lanciaDado);
+  sommaPc += lanciaDado;
+}
+console.log("contenuto dell'array PC: " + arrayPC);
+console.log('somma dei 3 dadi del PC: ' + sommaPc);
+
+//INIETTO LA SOMMA DEI DADI PC NELL'HTML
+document.getElementById("dado-pc-msg").innerHTML = sommaPc;
+
+//VERIFICO CHI HA VINTO
+if (sommaUser < sommaPc) {
+  console.log('HA VINTO IL PC !');
+  document.getElementById("winner-msg").innerHTML = ('HA VINTO IL PC !');
+} else if (sommaUser > sommaPc) {
+  console.log('HAI VINTO TU !');
+  document.getElementById("winner-msg").innerHTML = ('HAI VINTO TU !');
+} else {
+  console.log('PAREGGIO !');
+  document.getElementById("winner-msg").innerHTML = ('PAREGGIO !');
+}
+});
+
 
